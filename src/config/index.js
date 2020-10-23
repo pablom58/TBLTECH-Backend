dd27d0c039ae79d0df92ddf743f5db936babb32c
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-10-23 08:39:59 -0400

    Environment Variables

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/tbl_contacts',
     SECRET: process.env.SECRET || 'UE1WU19UQkxURUNIX1NFQ1JFVA',
-    USER_EMAIL: process.env.USER_EMAIL || 'tbltestbypmvs@gmail.com',
-    USER_EMAIL_PASSWORD: process.env.USER_EMAIL_PASSWORD || 'pmvstbl123'
+    USER_EMAIL: process.env.USER_EMAIL,
+    USER_EMAIL_PASSWORD: process.env.USER_EMAIL_PASSWORD
 }
